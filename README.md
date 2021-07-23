656ac2779d4b5fe56b8b097ef70ed8ccbfc42b5a
Huseyin OZDOGAN <huseyinozdogan@gmail.com>
2021-07-23 21:49:51 +0300

    Modified Battery full notification for POP_OS

---
+++ README.md
@@ -0,0 +1,41 @@
+# Original code is belongs to: 
+        https://github.com/hg8/battery-full-notification.git
+
+
+# Battery Full Notification
+Modified for Pop OS
+
+![preview](http://i.imgur.com/rVGMBK8.png)
+
+## Installation :
+
+1. Install `acpi` package :
+
+        sudo apt-get install acpi
+
+        sudo apt install libnotify-bin
+
+2. Clone this repo :
+        
+        git clone https://github.com/Dan88Hus/batteryFullNotificationPopOS.git
+        
+3. chmod +x batteryfull.sh
+
+4. Copy the script to ~/ folder: (any folder can be)
+
+4. Add the `batteryfull.sh` script as a startup application (Open Dash and search for `Startup Applications`) :
+
+    ![startup application](http://i.imgur.com/pWcq9TW.png)
+
+5. Done!
+
+
+----
+--------------
+PS:
+
+cp batteryfull.* ~/bin
+Or copy it to /usr/local/bin if you want it to be available for all users on your computer:
+
+
+    
---
+++ batteryfull.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+
+
+
+while true
+do
+    export DISPLAY=:0.0
+    battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+    if on_ac_power; then
+        if [ "$battery_percent" -gt 98 ]; then
+            notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
+            vlc /usr/share/sounds/freedesktop/stereo/ alarm-clock-elapsed.oga
+        fi
+    fi
+    sleep 100 # (5 minutes)
+done
+
+
+# --------------modified--------------------------
+# while true
+# do
+#     export DISPLAY=:0.0
+#     battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+#     if on_ac_power; then
+#         if [ "$battery_percent" -gt 92 ]; then
+#             notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
+            #paplay /usr/share/sounds/ubuntu/ringtones/Alarm\ clock.ogg
+            # vlc /usr/share/sounds/freedesktop/stereo\ alarm-clock-elapsed.oga
+#             vlc /usr/share/sounds/freedesktop/stereo/ alarm-clock-elapsed.oga
+#         fi
+#     fi
+#     sleep 300 # (5 minutes)
+# done
+
+
+
+
+
+#------------------original-----------------
+#!/bin/bash
+# while true
+# do
+#   battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+#    if [ $battery_level -ge 95 ]; then
+#       notify-send "Battery Full" "Level: ${battery_level}%"
+#       paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+#     elif [ $battery_level -le 20 ]; then
+#       notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
+#       paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+#   fi
+#  sleep 60
+# done
